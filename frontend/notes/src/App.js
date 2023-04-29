4bf9c8210cff078166321dee6be771f612b896bb
Pankaj-bit361 <pnkjvshst1@gmail.com>
2023-04-29 23:43:48 +0530

    done

--- frontend/notes/src/App.js
+++ frontend/notes/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
      <Mainroutes/>
+     <h1></h1>
      <Link to="/login"><button>Login</button></Link>
      <Link to="/signup"><button>Signup</button></Link>
     </div>
